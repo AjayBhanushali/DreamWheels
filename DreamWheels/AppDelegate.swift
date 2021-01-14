465c8645008fd94fd1f6d18707e6978c94f0abd1
Ajay Bhanushali <ajayrbhanushali@gmail.com>
2021-01-14 21:26:33 +0530

    Initial Commits

---
+++ DreamWheels/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  DreamWheels
+//
+//  Created by D2k on 14/01/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
